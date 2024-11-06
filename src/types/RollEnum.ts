7a791c78850926a0342fe6a7698af1159a0f1c39
aloekun <56211144+aloekun@users.noreply.github.com>
2024-11-07 00:46:19 +0900

    RollEnumクラスのswitchを連想配列に変更

--- src/types/RollEnum.ts
+++ src/types/RollEnum.ts
@@ -9,20 +9,18 @@ export enum RollEnum { // 役
 
 // 役の値から名称を取得
 export const getRollStr = (roll: RollEnum): string => {
-    switch (roll) {
-      case RollEnum.NONE:
-        return '目無し';
-      case RollEnum.ONLY_ONE_ROLL:
-        return '2つ一致、1つ残り';
-      case RollEnum.PINZORO:
-        return 'ピンゾロ';
-      case RollEnum.ARASHI:
-        return 'アラシ';
-      case RollEnum.HIFUMI:
-        return 'ヒフミ';
-      case RollEnum.SHIGORO:
-        return 'シゴロ';
-      default:
-        throw TypeError;
-    }
+  const rollStrings: { [key in RollEnum]: string } = {
+    [RollEnum.NONE]: '目無し',
+    [RollEnum.ONLY_ONE_ROLL]: '2つ一致、1つ残り',
+    [RollEnum.PINZORO]: 'ピンゾロ',
+    [RollEnum.ARASHI]: 'アラシ',
+    [RollEnum.HIFUMI]: 'ヒフミ',
+    [RollEnum.SHIGORO]: 'シゴロ',
+  };
+
+  if (roll in rollStrings) {
+    return rollStrings[roll];
+  } else {
+    throw new TypeError();
+  }
 };
