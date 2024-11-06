d84314957c4f28334a9ae0d0c7e59bd2bf2ed673
aloekun <56211144+aloekun@users.noreply.github.com>
2024-11-07 02:40:58 +0900

    ダイスを振る関数の複雑度を減らすリファクタリング

--- src/logic/JudgeLogic.ts
+++ src/logic/JudgeLogic.ts
@@ -1,73 +1,87 @@
-import { RollEnum } from "../types/RollEnum"
+import { RollEnum } from "../types/RollEnum";
 
 export enum ErrorJudge {
   NONE = 0,
   DICE_NUM_NOT_MUCH = 1,
 };
 
-/**
- * サイコロの出目の配列を受け取り、役を返す
- * 戻り値は役と目の値の配列。アラシや2つの目が一致した場合の目の大きさは、2つ目の要素で返す
- * 一二三や四五六がベタ書きの配列との比較なので、サイコロの出目の最大値を変更するなら要修正
- */
 export const judgeRoll = (dices: number[]): [RollEnum, number, ErrorJudge] => {
-    if (dices.length != 3) {
-      // 入力のサイコロ不足
-      return [RollEnum.NONE, 0, ErrorJudge.DICE_NUM_NOT_MUCH];
-    }
+  if (dices.length != 3) {
+    return [RollEnum.NONE, 0, ErrorJudge.DICE_NUM_NOT_MUCH];
+  }
 
-    // ヒフミか
-    const hifumiRolls: number[] = [1, 2, 3];
-    const hifumi: boolean = isArrayEqual(dices, hifumiRolls);
-    if (hifumi) {
-      return [RollEnum.HIFUMI, 0, ErrorJudge.NONE];
-    }
+  const rollType = getRollType(dices);
+  const diceOnlyOneRoll = getOnlyOneRoll(dices);
 
-    // シゴロか
-    const shigoroRolls: number[] = [4, 5, 6];
-    const shigoro: boolean = isArrayEqual(dices, shigoroRolls);
-    if (shigoro) {
-      return [RollEnum.SHIGORO, 0, ErrorJudge.NONE];
-    }
+  if (rollType !== RollEnum.NONE) {
+    return [rollType, 0, ErrorJudge.NONE];
+  }
 
-    // アラシか
-    const arashi: boolean = dices.every((value, index, array) => {
-      return value == array[0];
-    });
-    // ピンゾロか
-    const pinzoro: boolean = arashi && dices[0] == 1;
-    if (pinzoro) {
-      return [RollEnum.PINZORO, 0, ErrorJudge.NONE];
-    } else if (arashi) {
-      return [RollEnum.ARASHI, dices[0], ErrorJudge.NONE];
-    }
+  if (diceOnlyOneRoll > 0) {
+    return [RollEnum.ONLY_ONE_ROLL, diceOnlyOneRoll, ErrorJudge.NONE];
+  }
 
-    // 2つの目が一致、残り1つの目が役
-    let diceOnlyOneRoll = 0;
-    if (dices[0] == dices[1]) {
-      diceOnlyOneRoll = dices[2];
-    } else if (dices[1] == dices[2]) {
-      diceOnlyOneRoll = dices[0];
-    } else if (dices[0] == dices[2]) {
-      diceOnlyOneRoll = dices[1];
-    }
-    if (diceOnlyOneRoll > 0) {
-      return [RollEnum.ONLY_ONE_ROLL, diceOnlyOneRoll, ErrorJudge.NONE];
-    }
+  return [RollEnum.NONE, 0, ErrorJudge.NONE];
+};
 
-    // 役無し
-    return [RollEnum.NONE, 0, ErrorJudge.NONE];
-  };
-  // 数値配列の一致判定
-  const isArrayEqual = (arr1: number[], arr2: number[]): boolean => {
-    if (arr1.length != arr2.length) {
-      return false;
-    }
+const getRollType = (dices: number[]): RollEnum => {
+  if (isHifumi(dices)) {
+    return RollEnum.HIFUMI;
+  }
 
-    for (let i = 0; i < arr1.length; i++) {
+  if (isShigoro(dices)) {
+    return RollEnum.SHIGORO;
+  }
+
+  if (isPinzoro(dices)) {
+    return RollEnum.PINZORO;
+  }
+
+  if (isArashi(dices)) {
+    return RollEnum.ARASHI;
+  }
+
+  return RollEnum.NONE;
+};
+
+const isHifumi = (dices: number[]): boolean => {
+  const hifumiRolls: number[] = [1, 2, 3];
+  return isArrayEqual(dices, hifumiRolls);
+};
+
+const isShigoro = (dices: number[]): boolean => {
+  const shigoroRolls: number[] = [4, 5, 6];
+  return isArrayEqual(dices, shigoroRolls);
+};
+
+const isArashi = (dices: number[]): boolean => {
+  return dices.every((value, index, array) => value == array[0]);
+};
+
+const isPinzoro = (dices: number[]): boolean => {
+  return isArashi(dices) && dices[0] == 1;
+};
+
+const getOnlyOneRoll = (dices: number[]): number => {
+  if (dices[0] == dices[1]) {
+    return dices[2];
+  } else if (dices[1] == dices[2]) {
+    return dices[0];
+  } else if (dices[0] == dices[2]) {
+    return dices[1];
+  }
+  return 0;
+};
+
+const isArrayEqual = (arr1: number[], arr2: number[]): boolean => {
+  if (arr1.length != arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
       if (arr1[i] != arr2[i]) {
         return false;
-      }
     }
-    return true;
-  };
+  }
+  return true;
+}
