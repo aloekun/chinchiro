117b992cfd09f895eba81c2986cf16218e4f8161
aloekun <56211144+aloekun@users.noreply.github.com>
2024-11-10 02:49:24 +0900

    ちんちろりん1回分のサイコロ3つの出目を管理するクラスを追加

---
+++ src/logic/OneSetDices.ts
@@ -0,0 +1,39 @@
+import { rollDices } from "../components/DiceRoller";
+import { RollEnum } from "../types/RollEnum";
+import { ErrorJudge, judgeRoll } from "./JudgeLogic";
+
+/**
+ * ちんちろりん1回分のサイコロ3つの出目と役のセット
+ */
+export class OneSetDices {
+    private dices: number[];
+    private role: number;
+    private onlyOneNumber: number;
+
+    constructor() {
+        this.dices = [];
+        this.role = RollEnum.NONE;
+        this.onlyOneNumber = 0;
+    }
+
+    public getDices() {
+        return this.dices;
+    }
+
+    public getRole() {
+        return this.role;
+    }
+
+    public getOnlyOneNumber() {
+        return this.onlyOneNumber;
+    }
+
+    /**
+     * サイコロを振って、役を判定する
+     */
+    public rollDices() {
+        this.dices = rollDices();
+        let errorJudge:ErrorJudge = ErrorJudge.NONE;
+        [this.role, this.onlyOneNumber, errorJudge] = judgeRoll(this.dices);
+    }
+}
---
+++ src/logic/__tests__/OneSetDices.test.ts
@@ -0,0 +1,15 @@
+import { RollEnum } from "../../types/RollEnum";
+import { OneSetDices } from "../OneSetDices";
+
+test("1回分のダイスを振って、役が出る", () => {
+    const sut = new OneSetDices();
+
+    sut.rollDices();
+
+    const dices = sut.getDices();
+    const role = sut.getRole();
+    const onlyOneNumber = sut.getOnlyOneNumber();
+    expect(dices.length).toBe(3);
+    expect(role).toBeGreaterThanOrEqual(RollEnum.NONE);
+    expect(onlyOneNumber).toBeGreaterThanOrEqual(0);
+});
