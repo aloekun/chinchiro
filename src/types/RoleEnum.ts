0ff66853fc8381760a3a46ab60680fd095209ab7
aloekun <56211144+aloekun@users.noreply.github.com>
2024-11-13 04:20:13 +0900

    クラス内部の変数名を修正(roll -> role)

--- src/types/RoleEnum.ts
+++ src/types/RoleEnum.ts
@@ -9,8 +9,8 @@ export enum RoleEnum { // 役
 };
 
 // 役の値から名称を取得
-export const getRoleStr = (roll: RoleEnum): string => {
-  const rollStrings: { [key in RoleEnum]: string } = {
+export const getRoleStr = (role: RoleEnum): string => {
+  const roleStrings: { [key in RoleEnum]: string } = {
     [RoleEnum.NONE]: '未確定',
     [RoleEnum.ONLY_ONE_ROLL]: '2つ一致、1つ残り',
     [RoleEnum.PINZORO]: 'ピンゾロ',
@@ -20,8 +20,8 @@ export const getRoleStr = (roll: RoleEnum): string => {
     [RoleEnum.MENASHI]: '目無し',
   };
 
-  if (roll in rollStrings) {
-    return rollStrings[roll];
+  if (role in roleStrings) {
+    return roleStrings[role];
   } else {
     throw new TypeError();
   }
