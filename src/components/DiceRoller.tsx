9b536ce0489712aac786b2ee4d484963c6e60bd1
aloekun <56211144+aloekun@users.noreply.github.com>
2024-12-02 03:22:14 +0900

    不要になった単体のrollDices関数を削除

--- src/components/DiceRoller.tsx
+++ src/components/DiceRoller.tsx
@@ -4,18 +4,6 @@ type Props = {
   onClickPlay: () => void;
 };
 
-export const rollDices = (): number[] => {
-  const DICE_COUNT:number = 3;
-  const DICE_ROLES:number = 6;
-  const dices: number[] = [];
-  for (let i: number = 0; i < DICE_COUNT; i++) {
-    const dice = Math.floor(Math.random() * DICE_ROLES + 1);
-    dices.push(dice);
-  }
-  dices.sort((a, b) => a - b);
-  return dices;
-};
-
 export const DiceRoller: FC<Props> = ({ onClickPlay }) => {
   const handleClick = () => {
     onClickPlay();
